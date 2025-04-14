317fffce35943eeaf55528467e8b5b8e4d615676
David Morales <74932387+David-Morales-M@users.noreply.github.com>
2025-04-13 23:37:03 -0500

    DMO-13042025-feat: Presentation-card

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CardComponent} from './card/card.component';
 import {CertificatesComponent} from './certificates/certificates.component';
+import {ProjectsComponent} from './projects/projects.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardComponent,CertificatesComponent],
+  imports: [RouterOutlet, ProjectsComponent, CardComponent,CertificatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
