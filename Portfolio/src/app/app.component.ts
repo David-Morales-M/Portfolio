103e2aafe51e04ecdf944f0d8c8c88529a728637
David Morales <74932387+David-Morales-M@users.noreply.github.com>
2025-04-15 00:19:32 -0500

    DMO-15042025-feat: Navbar

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {CardComponent} from './card/card.component';
 import {CertificatesComponent} from './certificates/certificates.component';
 import {ProjectsComponent} from './projects/projects.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectsComponent, CardComponent,CertificatesComponent],
+  imports: [RouterOutlet, ProjectsComponent, CardComponent,CertificatesComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
